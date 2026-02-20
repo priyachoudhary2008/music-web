16ef81f9f307ad06ece892870c2d75f7a62c0d8c
pooja <poojachoudhary7181@gmail.com>
2026-02-20 10:49:30 +0530

    header file add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 
+import { useState } from 'react'
 import './App.css'
 import Header from "./components/header/Header"
 
 function App() {
-
+ const [location, setLocation] = useState("")
 
   return (
     <>
       <h1>App Components</h1>
       <Header/>
+      <h2>{location}</h2>
+
+      <input type="text" value={location} onChange={(e)=>{setLocation(e.target.value)}}/>
+     
+     
+
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,40 @@
-import "./Header.css"
 
-function Header (){
-    return(
-        <h1>Header Component</h1>
-    )
+import "./Header.css";
+import { BsBrowserEdge } from "react-icons/bs";
+
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">
+        
+        <span>
+        <BsBrowserEdge className="logo-icon" /><a href="#">My playlist</a>
+        </span>
+
+        <span>
+           <a href="#">Last Listening</a>
+        </span>
+      </div>
+      
+      <div className="search-bar">
+      
+        <input type="text" placeholder="What do you want to play?" />
+      </div>
+
+      <div className="header-right">
+        <a href="#">Premium</a>
+        <a href="#">Support</a>
+        <a href="#">Download</a>
+        <span className="divider">|</span>
+        <a href="#" className="install">
+        
+        </a>
+        <a href="#">Sign up</a>
+        <button className="login-btn">Log in</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
